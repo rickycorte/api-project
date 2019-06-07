47fce8f2983636c486c5e27f5eb1e7f20ac5980a
Riccardo <rickycortef@gmail.com>
2019-06-07 23:59:05 +0200

    Added more brt
    Former-commit-id: 3cdf1698c36394f2f9b860b1a03d25dc39863109

--- src/rbtree.c
+++ src/rbtree.c
@@ -16,7 +16,7 @@ typedef struct s_rbNode
 } rbNode;
 
 
-static void rb_LeftRotate(rbNode **root, rbNode* x)
+inline static void rb_LeftRotate(rbNode **root, rbNode* x)
 {
     rbNode *y = x->right;
 
@@ -36,7 +36,8 @@ static void rb_LeftRotate(rbNode **root, rbNode* x)
     x->parent = y;    
 }
 
-static void rb_RightRotate(rbNode **root, rbNode* x)
+
+inline static void rb_RightRotate(rbNode **root, rbNode* x)
 {
     rbNode *y = x->left;
 
@@ -57,11 +58,59 @@ static void rb_RightRotate(rbNode **root, rbNode* x)
 }
 
 
-static void rb_InsertFixup(rbNode **root, rbNode* z)
+inline static void rb_InsertFixup(rbNode **root, rbNode* z)
 {
     rbNode *x, *y;
+    while(z->parent->color == RED)
+    {
+        if(z->parent == z->parent->parent->left)
+        {
+            y = z->parent->parent->right;
+            if(y->color == RED)
+            {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            }
+            else
+            {
+                if(z == z->parent->right)
+                {
+                    z = z->parent;
+                    rb_LeftRotate(root, z);
+                }
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                rb_RightRotate(root, z->parent->parent);
+            }
+            
+        }
+        else
+        {
+            y = z->parent->parent->left;
+            if(y->color == RED)
+            {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            }
+            else
+            {
+                if(z == z->parent->left)
+                    {
+                        z = z->parent;
+                        rb_LeftRotate(root, z);
+                    }
+                    z->parent->color = BLACK;
+                    z->parent->parent->color = RED;
+                    rb_RightRotate(root, z->parent->parent);
+            }
+        }      
+    }
 
-
+    (*root)->color = BLACK;
 }
 
 
@@ -70,41 +119,91 @@ static void rb_InsertFixup(rbNode **root, rbNode* z)
  * @param rb_root radice dell'abero binario, puo puntare a null se l'abero va creato
  * @param data dati da inserire nel nodo
  */
-void rb_Insert(rbNode **rb_root, int data)
+inline void rb_Insert(rbNode **rb_root, int data)
 {
-    rbNode *z = (struct node*)malloc(sizeof(rbNode));
-    z->data = data;
-    z->left = NULL;
-    z->right = NULL;
-    z->parent = NULL;
+    rbNode *new_node = (struct node*)malloc(sizeof(rbNode));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->parent = NULL;
+    new_node->color = RED;
 
     if (*rb_root == NULL)
     {
-        z->color = BLACK;
-        (*rb_root) = z;
+        new_node ->color = BLACK;
+        (*rb_root) = new_node;
         return;
     }
     
+    //bst insertion
     rbNode *y = NULL;
     rbNode *x = *rb_root;
-
-    while(y != NULL)
+    while(x != NULL)
     {
         y = x;
-        if(z->data < x->data)
+        if(new_node ->data < x->data)
             x = x->left;
         else
             x = x->right;        
     }
-    z->parent = y;
-    if(z->data < y->data)
-        y->left = z;
-    else
-        y->right = z;
-    
-    z->left = NULL;
-    z->right = NULL;
-    z->color = RED;
+    new_node ->parent = y;
 
-    rb_InsertFixup(rb_root, z);
+    if(new_node->data < y->data)
+        y->left = new_node;
+    else
+        y->right = new_node;
+
+    rb_InsertFixup(rb_root, new_node);
+}
+
+
+/**
+ *  Cerca un elemento nell'albero binario
+ */
+inline rbNode *rb_Search(rbNode *rb_root, int val)
+{
+    rbNode *itr = rb_root;
+    while(itr != NULL && val != itr->data)
+    {
+        if(val < itr->data)
+            itr = itr->left;
+        else
+            itr = itr->right;
+    }
+
+    return itr;
+}
+
+
+inline static rbNode *rb_TreeSuccessor(rbNode* x)
+{
+    rbNode *y;
+
+    if(x->right != NULL)
+    {
+        while(x->right != NULL)
+            x = x->right;
+    }
+    y= x->parent;
+    while (y != NULL && x == y->right)
+    {
+        x = y;
+        y->parent = y;
+    }
+    
+    return y;
+    
+}
+
+inline void rb_Delete(rbNode **rb_root, int data)
+{
+    rbNode *z = rb_Search(rb_root, data);
+    if(!z) return 0;
+
+    rbNode *y;
+
+    if(z->left == NULL || z->right == NULL)
+    {
+
+    }
 }
